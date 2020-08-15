22dd99327142d62d54e376a8ec312ea00f7e7e5d
ngimb64 <56981761+ngimb64@users.noreply.github.com>
2020-08-15 12:05:40 -0400

    Update screenshotAssistant.py

--- screenshotAssistant.py
+++ screenshotAssistant.py
@@ -9,14 +9,14 @@ import time
 def on_press(key):
     global screenshot
 
-    if key == Key.esc:
+    if key == Key.esc or KeyboardInterrupt:
         screenshot.terminate()
         return False
 
 def screenshots(file_path):
     for x in range(0, 120):
         pic = ImageGrab.grab()
-        pic.save(file_path + str(random.randrange(1,500)) + '.png')
+        pic.save(file_path + str(random.randrange(1,600)) + '.png')
         time.sleep(5)
 
 def main():
@@ -24,19 +24,18 @@ def main():
 
     input('Please hit enter to begin\n')
 
-    key_listener = Listener(on_press=on_press) ; key_listener.start()
-    screenshot = Process(target=screenshots, args=(file_path,)) ; screenshot.start()
-    key_listener.join(600)
+    key_listener = Listener(on_press=on_press)
+    screenshot = Process(target=screenshots, args=(file_path,))
+    key_listener.start()
+    screenshot.start()
+    
+    key_listener.join(600.0)
     screenshot.join(timeout=600)
-
-    if screenshot is not None:
-        screenshot.terminate()
 
     main()
 
 
 if __name__ == '__main__':
-
     try:
         pathlib.Path('C:/Users/Public/Screenshots')\
                 .mkdir(parents=True, exist_ok=True)
